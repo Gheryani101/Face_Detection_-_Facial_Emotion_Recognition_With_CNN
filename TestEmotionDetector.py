c89775d30afdf680c2c4db6cf3a9e20023fd7c37
Mohamed khaled <48262254+Gheryani101@users.noreply.github.com>
2024-07-21 10:02:52 +0300

    test

---
+++ TestEmotionDetector.py
@@ -0,0 +1,44 @@
+import cv2
+import numpy as np
+from keras.models import model_from_json
+
+# Emotion dictionary
+emotion_dict = {0: "Angry", 1: "Disgusted", 2: "Fearful", 3: "Happy", 4: "Neutral", 5: "Sad", 6: "Surprised"}
+
+# Load the model
+with open('model/emotion_model.json', 'r') as json_file:
+    loaded_model_json = json_file.read()
+emotion_model = model_from_json(loaded_model_json)
+emotion_model.load_weights('model/emotion_model.h5')
+
+# open video
+#cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(r"D:\Machine Learning Project\Face_Detection_&_Facial_Emotion_Recognition_With_CNN\happy_video_25fps.mp4")
+
+# Load face detector
+face_detector = cv2.CascadeClassifier('haarcascades/haarcascade_frontalface_default.xml')
+
+while True:
+    ret, frame = cap.read()
+    frame = cv2.resize(frame, (1280, 720))
+    if not ret:
+        break
+    
+    gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    num_faces = face_detector.detectMultiScale(gray_frame, scaleFactor=1.3, minNeighbors=5)
+
+    for (x, y, w, h) in num_faces:
+        cv2.rectangle(frame, (x, y-50), (x+w, y+h+10), (0, 255, 0), 4)
+        roi_gray_frame = gray_frame[y:y + h, x:x + w]
+        cropped_img = np.expand_dims(np.expand_dims(cv2.resize(roi_gray_frame, (48, 48)), -1), 0)
+        
+        emotion_prediction = emotion_model.predict(cropped_img)
+        maxindex = int(np.argmax(emotion_prediction))
+        cv2.putText(frame, emotion_dict[maxindex], (x+5, y-20), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2, cv2.LINE_AA)
+
+    cv2.imshow('Emotion Detection', frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
